bf35f0782f56381fdbfe04dbb3ee4da0a091711b
ThomasR62875 <truijgt@itba.edu.ar>
2025-10-23 20:26:51 -0300

    feat: test_pcs funcional :)

--- Userland/userCodeModule/tests/test_processes.c
+++ Userland/userCodeModule/tests/test_processes.c
@@ -60,16 +60,16 @@ void test_processes_dummy(int argc, char **argv) {
     exit_pcs(ERROR);
   }
 
-  write_out("max_processes es ");
-  printDec(max_processes);
-  write_out("\n");
+  //write_out("max_processes es ");
+  //printDec(max_processes);
+  //write_out("\n");
   p_rq p_rqs[max_processes];
 
   while (1) {
 
     // Createmax_processes processes
     for (i = 0; i <max_processes; i++) {
-      p_rqs[i].pid = _create_process(&endless_loop, "endless_loop", 0, argvAux);
+      p_rqs[i].pid = _create_process(&endless_loop, "endless_loop", 0, argvAux); //aca usamos syscall porq no hay create_pcs dummy 
 
       if (p_rqs[i].pid == -1) {
         write_out("test_processes: ERROR creating process\n");
@@ -89,13 +89,19 @@ void test_processes_dummy(int argc, char **argv) {
         switch (action) {
           case 0:
             if (p_rqs[i].state == RUNNING || p_rqs[i].state == BLOCKED) {
-              if (_kill_process(p_rqs[i].pid) == -1) {
+              int argcK = 1;
+              char pid_str[21];
+              uint_to_str(p_rqs[i].pid, pid_str);
+              char *argvK[2];
+              argvK[0] = pid_str;
+              argvK[1] = NULL;
+              if (kill_process(argcK, argvK) == -1) {
                 write_out("test_processes: ERROR killing process\n");
                 exit_pcs(ERROR);
               }
-              write_out("Matamos a ");
-              printDec(p_rqs[i].pid);
-              write_out("\n");
+              //write_out("Matamos a ");
+              //printDec(p_rqs[i].pid);
+              //write_out("\n");
               p_rqs[i].state = KILLED;
               alive--;
             }
@@ -103,16 +109,21 @@ void test_processes_dummy(int argc, char **argv) {
 
           case 1:
             if (p_rqs[i].state == RUNNING) {
-              int ret = _block_process(p_rqs[i].pid);
-              if (ret == -1) {
+              int argcB = 1;
+              char pid_str[21];
+              uint_to_str(p_rqs[i].pid, pid_str);
+              char *argvB[2];
+              argvB[0] = pid_str;
+              argvB[1] = NULL;
+              if (block_process(argcB, argvB) == -1) {
                 write_out("test_processes: ERROR blocking process, pid no existe ");
                 printDec(p_rqs[i].pid);
                 write_out(". \n");
                 exit_pcs(ERROR);
               }
-              write_out("Bloqueamos a ");
-              printDec(p_rqs[i].pid);
-              write_out("\n");
+              //write_out("Bloqueamos a ");
+              //printDec(p_rqs[i].pid);
+              //write_out("\n");
               p_rqs[i].state = BLOCKED;
             }
             break;
@@ -122,20 +133,26 @@ void test_processes_dummy(int argc, char **argv) {
       // Randomly unblocks processes
       for (i = 0; i <max_processes; i++)
         if (p_rqs[i].state == BLOCKED && GetUniform(100) % 2) {
-          if (_unblock_process(p_rqs[i].pid) == -1) { //si o si esta bloqueado por el if de arriba
+          int argcU = 1;
+          char pid_str[21];
+          uint_to_str(p_rqs[i].pid, pid_str);
+          char *argvU[2];
+          argvU[0] = pid_str;
+          argvU[1] = NULL;
+          if (unblock_process(argcU, argvU) == -1) { //si o si esta bloqueado por el if de arriba
             write_out("test_processes: ERROR unblocking process\n");
             exit_pcs(ERROR);
           }
-          write_out("Desbloqueamos a ");
-          printDec(p_rqs[i].pid);
-          write_out("\n");
+          //write_out("Desbloqueamos a ");
+          //printDec(p_rqs[i].pid);
+          //write_out("\n");
           p_rqs[i].state = RUNNING;
         }
     }
 
     ProcessInfo* list = _get_proc_list();
-    write_out("\n=== Lista de procesos ===\n");
-    write_out("PID\tNombre\tEstado\tPPID\tRSP\tPrio\tChilds\tFD\n");
+    write_out("\n=== FAKE Lista de procesos ===\n");
+    write_out("PID\tNombre\tEstado\tPPID\n");
     write_out("-------------------------------------------------------------\n");
 
     //iterar sobre la lista
@@ -152,23 +169,13 @@ void test_processes_dummy(int argc, char **argv) {
         write_out("\t");
         if (p->parentPid == (uint64_t)-1) write_out("-1");
         else printDec(p->parentPid);
-        write_out("\t0x");
-        printHex((uint64_t)p->rsp); 
-        write_out("\t");
-        write_out(p->my_prio);
-        write_out("\t");
-        printDec(p->childrenAmount);
-        write_out("\t");
-        printDec(p->fileDescriptorCount);
         write_out("\n");
     }
 
     write_out("-------------------------------------------------------------\n");
 
     _free(list);
-    write_out(PROMPT_START3);
     exit_pcs(EXIT);
-
   }
 }
 
