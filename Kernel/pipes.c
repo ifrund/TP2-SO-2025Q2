1ffd9a84103119e0127460e368cbf7a361404666
jojohuerta <johuerta@itba.edu.ar>
2025-10-29 21:27:46 -0300

    pipe_close

--- Kernel/pipes.c
+++ Kernel/pipes.c
@@ -30,5 +30,46 @@ typedef struct {
     int is_in_use;
 } Pipe;
 
-// --- Tabla Global de Pipes ---
-Pipe global_pipe_table[MAX_PIPES];
+// Tabla Global de Pipes 
+Pipe global_pipe_table[MAX_PIPES] = {NULL};
+
+void pipe_init() {
+    // Inicializa el semaforo para la tabla global
+    sem_open_init("GLOBAL_PIPE_TABLE_LOCK", 1);
+    
+    // Marca todos los slots de pipes como libres
+    for (int i = 0; i < MAX_PIPES; i++) {
+        global_pipe_table[i].is_in_use = 0;
+    }
+}
+
+int pipe_close(int pipe_id) {
+    if (pipe_id < 0 || pipe_id >= MAX_PIPES) {
+        return -1; // PipeID invalido
+    }
+
+    sem_wait("GLOBAL_PIPE_TABLE_LOCK");
+
+    if (!global_pipe_table[pipe_id].is_in_use) {
+        sem_post("GLOBAL_PIPE_TABLE_LOCK");
+        return -1; // Pipe ya cerrado o invalido
+    }
+
+    Pipe* pipe = &global_pipe_table[pipe_id];
+
+    // Disminuir el contador de referencias
+    pipe->ref_count--;
+
+    //Si no hay mas referencias, liberar recursos
+    if (pipe->ref_count == 0) {
+        // Liberar recursos asociados al pipe
+        sem_close(pipe->sem_mutex_name);
+        sem_close(pipe->sem_full_name);
+        sem_close(pipe->sem_empty_name);
+        pipe->is_in_use = 0; // Marca el slot como libre
+    } 
+
+    sem_post("GLOBAL_PIPE_TABLE_LOCK");
+
+    return 0;
+}
