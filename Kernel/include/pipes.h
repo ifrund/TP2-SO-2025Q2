701e839bdfd723b311f0832256fc8a8863408382
ifrund <ifrund@itba.edu.ar>
2025-11-08 15:28:15 -0300

    Reapply "Merge branch 'PipesMvar'"
    This reverts commit 7f8726c9fed84721bd8ebef8ff09b194e8c3ee8a.

--- Kernel/include/pipes.h
+++ Kernel/include/pipes.h
@@ -3,6 +3,13 @@
 
 #include <stdint.h>
 
+#define STDIN 0
+#define STDOUT 1
+#define STDERROR 2
+
+#define PIPE_READ_END  0
+#define PIPE_WRITE_END 1
+
 // Creates or opens a pipe with the given 'name'. Returns a pipe identifier on success, or -1 on failure.
 int pipe_create_named(const char* name);
 
@@ -10,7 +17,7 @@ int pipe_create_named(const char* name);
 int pipe_create_anonymous(int pipe_ids[2]);
 
 // Closes the pipe identified by 'pipe_id'.
-int pipe_close(int pipe_id);
+int pipe_close(int pipe_id, int mode);
 
 // Writes up to 'count' bytes from 'buffer' into the pipe identified by 'pipe_id'.
 int pipe_write(int pipe_id, const char* buffer, int count);
@@ -19,5 +26,8 @@ int pipe_write(int pipe_id, const char* buffer, int count);
 int pipe_read(int pipe_id, char* buffer, int count);
 
 void pipe_init();
+
+// Register that a process has opened one end of the pipe. mode is PIPE_READ_END or PIPE_WRITE_END.
+int pipe_register(int pipe_id, int mode);
 
 #endif // _PIPES_H_
