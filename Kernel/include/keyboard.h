9f1ebb14686e8a956c0aa6652041452775632c61
ifrund <ifrund@itba.edu.ar>
2025-11-09 18:53:21 -0300

    pvs: keyboard.h

--- Kernel/include/keyboard.h
+++ Kernel/include/keyboard.h
@@ -4,6 +4,6 @@ void key_handler();
 int read_key(int fd);
 void insert_key(int key);
 void flush_buffer();
-void checkShift();
+void checkShift(int key);
 void checkRegs(int key);
 int altPressed();
